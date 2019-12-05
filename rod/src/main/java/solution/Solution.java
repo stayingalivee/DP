a5148bb607b3f4b3e15a3fbd5252f833be34a221
Ahmad <n.ahmadalkhatib@gmail.com>
2019-12-06 00:40:37 +0300

    dk

--- rod/src/main/java/solution/Solution.java
+++ rod/src/main/java/solution/Solution.java
@@ -16,16 +16,13 @@ class Solution{
 		if(n==0) {
 			return 0;
 		}
-
 		int q = -1;
 		for (int i=1;i<=n;i++){
-		
 			q = Math.max(q, p[i-1]+ cut_rod(p, n-i));
 		}
 		
 		return q;
 	}
-
 
 	// Dynamic programming approach, store the value and return it if exist.
 	public int memoized_cut_rod(int[]p, int n, int []r){
@@ -48,10 +45,9 @@ class Solution{
 	// Dynamic progrmming approach, no recursion, the solution is built up from a base case.
 	public int bottom_up_cut_road(int[] p, int n)
 	{
-			
+		
 		int[] r = new int[n+1];
 		r[0] = 0;
-
 		for(int i=1; i<=n; i++){
 
 			int q=-1;
@@ -66,7 +62,7 @@ class Solution{
 	public static void main(String[] args) {
 		
 		int[] p = {1,5,8,9,10,17,17,20,24,30};
-		int[] r = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+		//int[] r = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}; // uncomment when memoized_cut_rod is called
 
 		int q = new Solution().bottom_up_cut_road(p, 10);
 		System.out.println(q);
