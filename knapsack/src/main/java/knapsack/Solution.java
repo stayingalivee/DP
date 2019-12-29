7efe19863fcd1ad787debed8ea78221c4808ffc1
Ahmad <n.ahmadalkhatib@gmail.com>
2019-12-29 19:00:27 +0300

    add explanation for knapsack

--- knapsack/src/main/java/knapsack/Solution.java
+++ knapsack/src/main/java/knapsack/Solution.java
@@ -58,6 +58,7 @@ import javax.xml.crypto.dsig.keyinfo.KeyName;
  * | | 4 | 0 |   |   |   |   |   |   |   |   |   |   |
  * v +---+---+---+---+---+---+---+---+---+---+---+---+
  * 
+
  *
  * At first glance we can build our base case where n = 0 and w = 0, for those cases
  * the total value is plain 0, hence we filled those rows and columns with only 0s.
@@ -70,10 +71,93 @@ import javax.xml.crypto.dsig.keyinfo.KeyName;
  * 
  * Now for n = 1 we are solving when we only have 1 item to pick, that is 
  * the item V = 10 with W = 5. The item's weight = 5 so for j = 0->4
- * we cannot put that item in the bag, 
+ * we cannot put that item in the bag, then starting from j = 5, we can actually 
+ * start making our decision, we have 2 options:
+ *      1. put the item.
+ *      2. don't
+ * In this case, it's pretty obvious that putting the item in the bag will maximize
+ * the value (10 > 0, DUUH), and we get the following table
+
+ *         <----------------- W ---------------------> 
+ *   +---+---+---+---+---+---+---+---+---+---+---+---+
+ *   |   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10|
+ *   +---+---+---+---+---+---+---+---+---+---+---+---+
+ * ^ | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+ * | +---+---+---+---+---+---+---+---+---+---+---+---+
+ * | | 1 | 0 | 0 | 0 | 0 | 0 | 10| 10| 10| 10| 10| 10|
+ * | +---+---+---+---+---+---+---+---+---+---+---+---+
+ * | | 2 | 0 |   |   |   |   |   |   |   |   |   |   |
+ * n +---+---+---+---+---+---+---+---+---+---+---+---+
+ * | | 3 | 0 |   |   |   |   |   |   |   |   |   |   |
+ * | +---+---+---+---+---+---+---+---+---+---+---+---+
+ * | | 4 | 0 |   |   |   |   |   |   |   |   |   |   |
+ * v +---+---+---+---+---+---+---+---+---+---+---+---+
  * 
+ * ! so far so good
  * 
+ * Now we get to n=2, which means we have 2 items to fill, BUT we already SOLVED
+ * for n = 1 and we know for sure which is the best decision for that item with the
+ * given max weight capacity, so we're gonna use that information to help us solve
+ * for n = 2!!. But in more general terms, let's actually not talk about item #2 
+ * let's generalize and solve for item #i
  * 
+ * 1. To calculate the maximum value for item i, we first need to compare the weight
+ * for the item i with maximum weight capacity, if the item i weights more than the
+ * maximum capacity we cannot include it. Hence making the calculations doesn't make sense
+ * and in that case, the solution for the problem is whatever value we obtain without 
+ * chosing the item i, technically, it's the value in the row above with the same column.
+ * 
+ * 2. However, if the weight for item i is less than the max capacity then we have
+ * the option to include that item. if it potentially increases the maximum obtainable
+ * value then we can chose it. 
+ * ? The maximum optainable value after including item i is:
+ * ?     The value of item i itself + the maximum value that can be obtained
+ * !     with the remaining capacity of our knapsack.
+ * 
+ * therefore at row i column j (which represents the maximum value we can obtain there)
+ * ! we would pick either the maximum value that we can obtain without item i,
+ * ! or the maximum value that we can obtain with item 1, whichever is larger.
+ * 
+ * In the below table, row 3 column 5 (item 2 with max capacity of 4), we can chose
+ * to either include the item 2 (which weighs 4 units) or not. if we choose NOT to 
+ * onclude it, the maximum value we can obtain is the same as if we only have item 1
+ * to choose from (which is found in the row above). If we want to include item 2
+ * (value 40 weight 4), the maximum value we can obtain with item 2 is the value of
+ * item 2 (40) + the maximum value we can obtain with the REMAINING capacity
+ * 
+ * at row 3 column 10 (item 10 capacity 9), again, we can choose to either
+ * include item 2 or not. if we choose not to, the maximum value we can obtain is
+ * the value in the row above it, (by including only item 1 of value 10). If we
+ * choose to include item 2 (value 40, weight 4), then we have remaining capacity 
+ * of 9 - 4 (we are at capacity 9) = 5.
+ * ! WE ALREADY SOLVED FOR MAX CAPACITY = 5, WE CAN USE THAT VALUE IN OUR CALCULATION
+ * (AND THAT IS THE CRUX OF DYNAMIC PROGRAMMING!!!!!!!!!!!!!!!!!!!!!!!!!!!)
+ *  We can find out the maximum value that can be obtained with a capacity of 5 by
+ * simply looking at the row above were j = 5. Thus, the maximum value we can obtain
+ * by including item 2 is 40 (the value of item 2) + 10 = 50
+ * now we compare our new value (50) with the older value (10) and pick whichever 
+ * is larger. and we have the following table updated
+ * 
+ *
+ *         <----------------- W ---------------------> 
+ *   +---+---+---+---+---+---+---+---+---+---+---+---+
+ *   |   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10|
+ *   +---+---+---+---+---+---+---+---+---+---+---+---+
+ * ^ | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+ * | +---+---+---+---+---+---+---+---+---+---+---+---+
+ * | | 1 | 0 | 0 | 0 | 0 | 0 | 10| 10| 10| 10| 10| 10|
+ * | +---+---+---+---+---+---+---+---+---+---+---+---+
+ * | | 2 | 0 | 0 | 0 | 0 | 40| 40| 40| 40| 40| 50| 50|
+ * n +---+---+---+---+---+---+---+---+---+---+---+---+
+ * | | 3 | 0 |   |   |   |   |   |   |   |   |   |   |
+ * | +---+---+---+---+---+---+---+---+---+---+---+---+
+ * | | 4 | 0 |   |   |   |   |   |   |   |   |   |   |
+ * v +---+---+---+---+---+---+---+---+---+---+---+---+
+ * 
+ *
+ * Now go ahead and fill the rest of the table.
+ * 
+ * ----------------------------------------------------------------------
  */
 
 
