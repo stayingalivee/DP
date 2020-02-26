4c7f62c0e9c2c6b01c40a2050785cf619bf7d4b1
Ahmad <n.ahmadalkhatib@gmail.com>
2020-02-26 22:58:44 +0300

    add LCS

---
+++ longest-common-sequence/ChildString.java
@@ -0,0 +1,37 @@
+package main.java.com.deadbeef.hackerrank.childstring;
+
+class ChildString{
+
+
+    // Complete the commonChild function below.
+    static int commonChild(String s1, String s2) {
+
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+
+        for(int i=0;i<dp.length;i++) dp[i][0] = 0;
+        for(int i=0;i<dp[0].length;i++) dp[0][0] = 0;
+
+        for(int i=1;i<dp.length;i++){
+            for(int k=1; k<dp[0].length; k++){
+                
+                if(s1.charAt(i-1) == s2.charAt(k-1))
+                    dp[i][k] = dp[i-1][k-1]+1;
+                else
+                    dp[i][k] = Math.max(dp[i-1][k], dp[i][k-1]);
+
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+
+    }
+
+
+    public static void main(String[] args) {
+
+        String s1 = "SHINCHAN";
+        String s2 = "NOHARAAA";
+        int res = commonChild(s1, s2);
+        System.out.println(res);
+    }
+}
