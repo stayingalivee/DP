63e11df5fbe9f7d67c10663d03b003cc4b8720e5
Ahmad <n.ahmadalkhatib@gmail.com>
2020-04-09 20:07:32 +0300

    best way to rob houses

---
+++ leetCode/RobHouses.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+class Solution{
+
+    public static int rob(int[] nums) {
+
+        if (nums.length == 0) return 0;
+
+        int[] memo = new int[nums.length + 1];
+        memo[0] = 0;
+        memo[1] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int val = nums[i];
+            memo[i+1] = Math.max(memo[i], memo[i-1] + val);
+        }
+        return memo[nums.length];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,0,0,2};
+        System.out.println(rob(nums));
+    }
+}
